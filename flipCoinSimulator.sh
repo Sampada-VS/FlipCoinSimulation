f4fbfd8ac5cb4804b2f9ffe91996b62434a4cf01
Sampada-VS <samssvv1@gmail.com>
2020-08-05 19:23:19 +0530

    Adding feature-flip coin and display number of wins for head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
 HEADS=1
-flip_coin=$((RANDOM%2))
+NUM_OF_FLIPS=10
 
-if [ $flip_coin -eq $HEADS ]
-then
-        echo "Heads is winner."
-else
-        echo "Tails is winner."
-fi
+head_win=0
+tail_win=0
+
+for (( i=1; i<=$NUM_OF_FLIPS; i++ ))
+do
+        flip_coin=$((RANDOM%2))
+        if [ $flip_coin -eq $HEADS ]
+        then
+                head_win=$(($head_win+1))
+        else
+                tail_win=$(($tail_win+1))
+        fi
+done
+echo "Heads win :" $head_win "Tails win :" $tail_win
