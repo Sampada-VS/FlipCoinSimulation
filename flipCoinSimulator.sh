2e2569a00cfc82c36cb3440a8c0613283c8f6ac6
Sampada-VS <samssvv1@gmail.com>
2020-08-05 18:10:41 +0530

    Adding feature-flip coin to display winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
-echo "Welcome to flip coin simulator program."
+HEADS=1
+flip_coin=$((RANDOM%2))
+
+if [ $flip_coin -eq $HEADS ]
+then
+        echo "Heads is winner."
+else
+        echo "Tails is winner."
+fi
