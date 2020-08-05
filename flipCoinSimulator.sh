5224303a71ed3b2ec702fd197d6371965436777f
Sampada-VS <samssvv1@gmail.com>
2020-08-05 19:49:50 +0530

    Adding feature-flip coin and display win difference

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 HEADS=1
-NUM_OF_FLIPS=10
+NUM_OF_FLIPS=21
 
 head_win=0
 tail_win=0
 
-for (( i=1; i<=$NUM_OF_FLIPS; i++ ))
+while [[ $head_win -lt $NUM_OF_FLIPS && $tail_win -lt $NUM_OF_FLIPS ]]
 do
         flip_coin=$((RANDOM%2))
         if [ $flip_coin -eq $HEADS ]
@@ -15,4 +15,13 @@ do
                 tail_win=$(($tail_win+1))
         fi
 done
-echo "Heads win :" $head_win "Tails win :" $tail_win
+
+if [ $head_win -eq $tail_win ]
+then
+        echo "It's a Tie."
+elif [ $head_win -gt $tail_win ]
+then
+        echo "Head won by $(($head_win-$tail_win))."
+else
+        echo "Tail won by $(($tail_win-$head_win))."
+fi
